2a650ac1034b23f9bc7a255ede1a560a2b490900
MacLier <75802094+MacLier@users.noreply.github.com>
2021-06-06 12:41:02 +0200

    feat: first view testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  description: string = "This is ToDone app first view"
   title = 'ToDone';
 }
