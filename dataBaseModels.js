a95f8f304a693a27231a7423125dff53b4520aa3
MacLier <75802094+MacLier@users.noreply.github.com>
2021-06-15 10:20:11 +0200

    feat: sequelize & dataModels added

---
+++ dataBaseModels.js
@@ -0,0 +1,68 @@
+export class Users {
+    ID;
+    UUID;
+    firstName;
+    lastName;
+    nickName;
+    email;
+    role;
+}
+
+export class UserTasks {
+    userID;
+    taskID;
+}
+
+export class Tasks {
+    ID = 1;
+    name;
+    description;
+    allDone;
+    timeRequirement;
+    index;
+}
+
+export class Events {
+    ID;
+    taskID;
+    isItDone;
+    allDone;
+    deadline;
+    softDeadline;
+    notifications;
+    members;
+    location;
+}
+export class Preparations {
+    ID;
+    eventID;
+    preparationName;
+    isItDone
+}
+export class Todos {
+    ID;
+    taskID;
+    isItDone;
+    allDone;
+    deadline;
+    softDeadline;
+    startDate;
+}
+export class Steps {
+    ID;
+    todoID;
+    stepName;
+    isItDone;
+}
+export class shoppingLists {
+    ID;
+    taskID
+}
+export class Products {
+    ID;
+    shoppingListID;
+    productName;
+    measure;
+    amount;
+    isItDone;
+}
